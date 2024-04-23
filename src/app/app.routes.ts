e41984c4dba7e22ec0e0f14d5dd2be25effe81a6
mohamedesmat1997 <158211063+63456356@users.noreply.github.com>
2024-04-23 02:00:15 +0200

    login style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login';
+import { SignupComponent } from './signup';
 
 export const routes: Routes = [
-    {path:'',component:HomeComponent}
+    {path:'',component:HomeComponent},
+    {path:'signup',component:SignupComponent},
+    {path:'login',component:LoginComponent}
 ];
