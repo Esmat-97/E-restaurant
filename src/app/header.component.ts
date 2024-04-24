0d60add16f289ed7b52584f3c7c7d1780796b69b
mohamedesmat1997 <158211063+63456356@users.noreply.github.com>
2024-04-24 07:46:10 +0200

    guest page

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,36 +1,42 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
-  imports: [RouterOutlet,RouterLink],
+  imports:[RouterLink, CommonModule],
+  standalone:true,
   template: `
-  
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" routerLink="/logout">Logout</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" routerLink="/">Home</a>
+            <a class="nav-link" routerLink="/features">Features</a>
+            @if( this.role){
+            <a class="nav-link" routerLink="/logout">Logout</a>
+            }
+            
+            @if(!this.role){
+            <a class="nav-link" routerLink="/login">Login</a>
+            }
+          </div>
+        </div>
       </div>
-    </div>
-
-  </div>
-</nav>
-  
+    </nav>
   `,
   styles: [``]
 })
 export class HeaderComponent {
   title = 'myApp';
+
+  role:string='';
+
+  ngOnInit(){
+  this.role=  localStorage.getItem('role') as string
+  }
 }
