efd874dc8182a6e3de420bae2c6273097d261e4e
mohamedesmat1997 <158211063+63456356@users.noreply.github.com>
2024-04-25 07:16:39 +0200

    roles

--- src/app/products.ts
+++ src/app/products.ts
@@ -12,15 +12,29 @@ import { CommonModule } from '@angular/common';
 
 
   <div class="col" *ngFor="let user of data , let i =index">
-    <div class="card h-100">
+    <div class="card ">
       <img [src]="'../assets/' +user.image" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">{{user.product_name}}</h5>
         <p class="card-text">{{user.price}} LE</p>
+
+        @if(role === 'admin'){
         <button class="btn btn-danger"> delete</button>
         <button class="btn btn-warning"> update</button>
         <button class="btn btn-info"> show more</button>
         <button class="btn btn-light"> Add to cart</button>
+        }
+
+        
+        @if( ! role){
+         
+          }
+  
+        @if(role === 'customer'){
+        <button class="btn btn-info"> show more</button>
+        <button class="btn btn-light"> Add to cart</button>
+        }
+      
       </div>
     </div>
 
@@ -36,11 +50,14 @@ import { CommonModule } from '@angular/common';
 export class ProductsComponent {
   title = 'myApp';
 data:any=[];
+role:string='';
 
   constructor(private htp:HttpClient){}
 
   ngOnInit(){
-      
+  
+     this.role = localStorage.getItem('role') as string;
+    
 this.htp.get('http://localhost:1999/products').subscribe((res:any)=>{
     this.data=res;
 console.log(this.data)
