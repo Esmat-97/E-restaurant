dfc3ef3dca8c9beabe3055117d5c4ce1f78e91c1
mohamedesmat1997 <158211063+63456356@users.noreply.github.com>
2024-04-28 07:16:19 +0300

    add user validation

--- src/app/addusers.ts
+++ src/app/addusers.ts
@@ -11,30 +11,60 @@ import { FormsModule } from '@angular/forms';
 
   <div class="col-6">
   <label for="inputAddress" class="form-label">first name</label>
-  <input type="text" class="form-control" name="fname"  #fname="ngModel"  ngModel>
+  <input type="text" class="form-control" name="fname"  #fname="ngModel" required  ngModel>
+
+  @if(fname.invalid && fname.touched){
+    @if(fname.errors?.['required']){
+        <span> the first name is required </span>
+    }
+  }
 </div>
 
   <div class="col-md-6">
     <label for="inputEmail4" class="form-label">last name</label>
-    <input type="text" class="form-control" name="lname"  #lname="ngModel"  ngModel>
+    <input type="text" class="form-control" name="lname"  #lname="ngModel" required ngModel>
+
+  @if(lname.invalid && lname.touched){
+    @if(lname.errors?.['required']){
+        <span> the second name is required </span>
+    }
+  }
   </div>
 
 
   <div class="col-12">
   <label for="inputAddress2" class="form-label">Email</label>
-  <input type="email" class="form-control" name="email"  #email="ngModel" ngModel>
+  <input type="email" class="form-control" name="email"  #email="ngModel" required ngModel>
+
+  @if(email.invalid && email.touched){
+    @if(email.errors?.['required']){
+        <span> the email is required </span>
+    }
+  }
 </div>
 
 
   <div class="col-md-6">
     <label for="inputPassword4" class="form-label">Password</label>
-    <input type="password" class="form-control" name="password"   #password="ngModel"  ngModel>
+    <input type="password" class="form-control" name="password"   #password="ngModel" required  ngModel>
+
+    @if(password.invalid && password.touched){
+        @if(password.errors?.['required']){
+            <span> the password is required </span>
+        }
+      }
   </div>
 
 
   <div class="col-md-6">
     <label for="inputCity" class="form-label">phone</label>
-    <input type="text" class="form-control" name="phone"  #phone="ngModel" ngModel>
+    <input type="text" class="form-control" name="phone"  #phone="ngModel" required ngModel>
+
+    @if(phone.invalid && phone.touched){
+        @if(phone.errors?.['required']){
+            <span> the phone is required </span>
+        }
+      }
   </div>
 
 
@@ -56,7 +86,7 @@ export class AddusersComponent {
   formdata(main :any){
 
     console.log(main.value)
-    
+
     this.gue.insertguests(main.value).subscribe(res=>{
 
     })
